7e5da82c95195126ef77fd275f473f28ef051ab0
Justin Kunimune <jkunimune15@punahou.edu>
2015-08-10 12:09:55 +1000

    Literacy
    The program now has the capacity to read standard mathematical notation.
    This enables it to take user input, completing the interactive portion.
    It is not perfect, as it does not yet recognize expressions such as
    "2x", but I intend to work those out soon.

--- IntegrationTest.java
+++ IntegrationTest.java
@@ -1,19 +1,26 @@
+import java.util.*;
+
+
+
 public class IntegrationTest {
   public static void main(String[] args) {
-    for (int i = 1; i <= 20; i +=2) {
-      Function f = new Function(i);
-      System.out.print("f(x) = "+f.ofX());
-      f.simplify();
-      System.out.print(" = "+f.ofX());
-      System.out.println(" and f(0) = "+f.of(0));
-      
-      Function g = f.prime();
-      System.out.print("f'(x) = "+g.ofX());
-      f.simplify();
-      System.out.print(" = "+g.ofX());
-      System.out.println(" and f'(0) = "+g.of(0));
-      
-      System.out.println();
-    }
+    Scanner in = new Scanner(System.in);
+    Function f;
+    do {
+      f = new Function(in.nextLine(), true);
+    } while (!f.isValid());
+    
+    System.out.print("f(x) = "+f.ofX());
+    f.simplify();
+    System.out.print(" = "+f.ofX());
+    System.out.println(" and f(0) = "+f.of(0));
+    
+    Function g = f.prime();
+    System.out.print("f'(x) = "+g.ofX());
+    f.simplify();
+    System.out.print(" = "+g.ofX());
+    System.out.println(" and f'(0) = "+g.of(0));
+    
+    System.out.println();
   }
 }
--- IntegrationTest.java~
+++ IntegrationTest.java~
@@ -1,14 +1,19 @@
 public class IntegrationTest {
   public static void main(String[] args) {
-    for (int i = 1; i <= 20; i ++) {
+    for (int i = 1; i <= 20; i +=2) {
       Function f = new Function(i);
-      System.out.println("The function is "+f.string);
-      System.out.print("f(x)="+f.ofX());
-      System.out.println(" and f(0)="+f.of(0));
+      System.out.print("f(x) = "+f.ofX());
       f.simplify();
-      System.out.println("The function simplifies to "+f.string+" Now,");
-      System.out.print("f(x)="+f.ofX());
-      System.out.println(" and f(0)="+f.of(0)+"\n");
+      System.out.print(" = "+f.ofX());
+      System.out.println(" and f(0) = "+f.of(0));
+      
+      Function g = f.prime();
+      System.out.print("f'(x) = "+g.ofX());
+      f.simplify();
+      System.out.print(" = "+g.ofX());
+      System.out.println(" and f'(0) = "+g.of(0));
+      
+      System.out.println();
     }
   }
 }
